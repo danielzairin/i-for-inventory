df1a89510d87aff047f3c4e12921dc7b7295f67d
Daniel Zairin <danielzairink@gmail.com>
2024-03-05 21:28:57 +0800

    Patch weird delayed libsql import

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
+/* Import @libsql/client early. Otherwise, it delays initial requests to our API.
+   I suspect it's making a network request for some reason on first import. */
+import("@libsql/client");
+
 const nextConfig = {};
 
 export default nextConfig;
